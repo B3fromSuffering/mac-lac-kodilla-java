e4f05c6d1ef50e9cef2d84dd5baee4e10ba5e503
B3fromSuffering <laczny.maciej@gmail.com>
2019-12-23 10:10:02 +0100

    poem decorator

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,4 +1,39 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
 public class StreamMain {
+    public static void main(String[] args) {
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        //Zadanie: Upiększacz Tekstów
+
+        System.out.println("\nPoem Beautifier");
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("Nor dread nor hope attend", (stringToDecorate -> stringToDecorate.toUpperCase()));
+        poemBeautifier.beautify("A dying animal;", (stringToDecorate -> "ABC " + stringToDecorate + " ABC"));
+        poemBeautifier.beautify("A man awaits his end", (stringToDecorate -> ">>> " + stringToDecorate + " <<<"));
+        poemBeautifier.beautify("Dreading and hoping all", (stringToDecorate -> stringToDecorate.replace("i", "!")));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
+
+    }
+
+
 }
--- kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
@@ -1,4 +1,9 @@
 package com.kodilla.stream.beautifier;
 
+import com.kodilla.stream.lambda.Executor;
+
 public class PoemBeautifier {
+    public void beautify (String stringToDecorate, PoemDecorator poemDecorator) {
+         System.out.println(poemDecorator.decorate(stringToDecorate));
+    }
 }
