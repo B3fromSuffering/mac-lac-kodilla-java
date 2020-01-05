d2263beb327efecdfc98b01141d07b890487433d
unknown <laczny.maciej@gmail.com>
2020-01-05 16:28:51 +0100

    7.4 ilosc ludzi na swiecie

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/World.java
@@ -0,0 +1,23 @@
+package com.kodilla.stream.world;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class World {
+    private List<Continent> continentList = new ArrayList<>();
+
+    public void addContinent(Continent continent){
+        continentList.add(continent);
+    }
+
+    public List<Continent> getTheListOfContinents() {
+        return continentList;
+    }
+    public BigDecimal getPeopleQuantity(){
+        return continentList.stream()
+                .flatMap(continent -> continent.getTheListOfCountries().stream())
+                .map(Country::getPeopleQuantity)
+                .reduce(BigDecimal.ZERO, (sum, current) -> sum = sum.add(current));
+    }
+}
